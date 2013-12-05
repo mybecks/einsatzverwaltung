81044698782ecc536188bd6ebb262c6b14b1ba7e
mybecks <andre_becker@gmx.net>
2013-12-05 18:54:52 +0100

    embedding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+	"use strict";
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+	});
+
+	// Load the plugin that provides the "uglify" task.
+	// grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Default task(s).
+	// grunt.registerTask('default', ['uglify']);
+
+};
