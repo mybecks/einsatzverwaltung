2344241dde8f3a00fffa52ca344f24d72250faf9
mybecks <andre_becker@gmx.net>
2013-12-18 14:55:19 +0100

    nearly complete precommit hook

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function(grunt) {
     var path = require('path');
     var fs = require('fs');
     // var versioning = require('versioning');
-
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -17,38 +16,59 @@ module.exports = function(grunt) {
         var packageJson = grunt.file.readJSON('package.json');
         var einsatzverwaltung = readFile('einsatzverwaltung.php');
         var readme = readFile('Readme.md');
+        var version = incrementVersion(packageJson);
 
+        // stream = fs.createReadStream(path.join('einsatzverwaltung.php'), {encoding: 'utf8'});
+        // stream.on('readable', read);
+        // stream.once('end', function() {
+        //     console.log('stream ended');
+        // });
+        // updateVersion(packageJson, version);
+        updateVersion(einsatzverwaltung, version);
+        // updateVersion(readme, version);
 
-        console.log(packageJson+'\n'+einsatzverwaltung+'\n'+readme);
+        // console.log(packageJson+'\n'+einsatzverwaltung+'\n'+readme);
     });
 
+    // var read = function() {
+    //     var buf;
+    //     while (buf = stream.read()) {
+    //         console.log('Read from the file:', buf);
+    //     }
+    // };
+
     var readFile = function(file){
-        var ext = path.extname(file);
-        if(ext === '.php'){
-            getVersion(file);
-            return 'php';
-        }else if(ext === '.md'){
-            getVersion(file);
-            return 'md';
-        }else{
-            return ext+" not supported";
-        }
+        return fs.readFileSync(file,'utf8');
     };
 
-    var updateVersion = function(file){
-        var php = fs.readFileSync(file,'utf8');
-        var regex = /\d*.\.\d*.\.\d*/g;
-
-        var version = regex.exec(php);
-        console.log('version: '+version);
-        return version;
+    var writeFile = function(filePath, content){
+        fs.writeFileSync(fileName, content, 'utf8');
     };
 
-    var incrementVersion = function(oldVersion){
+    var updateVersion = function(file, filePath, newVersion){
+        var regex = /\d*.\.\d*.\.\d*/;
 
-        var version = oldVersion.split('.');
+
+
+
+
+        var oldVersion = regex.exec(file);
+        console.log('old version: '+oldVersion);
+        console.log('new version: '+newVersion);
+
+        file = file.replace(regex, newVersion);
+
+        console.log(file);
+
+        // return version;
+    };
+
+    var incrementVersion = function(file){
+
+        var version = file.version.split('.');
         var versionNumber = parseInt(version[2], 10);
-        return version[0]+'.'+version[1]+'.'+versionNumber++;
+        versionNumber++;
+        return version[0]+'.'+version[1]+'.'+versionNumber;
     };
 };
 
