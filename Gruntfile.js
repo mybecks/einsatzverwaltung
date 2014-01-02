219388e6ba5009d404d7b76096c2c42446acf0c4
mybecks <andre_becker@gmx.net>
2014-01-02 15:26:09 +0100

    raise version preparements

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,8 @@ module.exports = function(grunt) {
 
     var path = require('path');
     var fs = require('fs');
-    // var versioning = require('versioning');
-	// Project configuration.
+    var exec = require('child_process').exec;
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		
@@ -13,78 +13,92 @@ module.exports = function(grunt) {
     grunt.registerTask('default', ['precommit']);
 
     grunt.registerTask('precommit', '', function(){
-        var packageJson = grunt.file.readJSON('package.json');
-        var einsatzverwaltung = readFile('einsatzverwaltung.php');
-        var readme = readFile('Readme.md');
+        var packageJson = {
+            content: grunt.file.readJSON('package.json'),
+            filePath: path.join(__dirname, 'package.json')
+        };
+        var einsatzverwaltung = {
+            content: readFile('einsatzverwaltung.php'),
+            filePath: path.join(__dirname, 'einsatzverwaltung.php')
+        };
+        var readme = {
+            content: readFile('Readme.md'),
+            filePath: path.join(__dirname, 'Readme.md')
+        };
+        
         var version = incrementVersion(packageJson);
 
-        // stream = fs.createReadStream(path.join('einsatzverwaltung.php'), {encoding: 'utf8'});
-        // stream.on('readable', read);
-        // stream.once('end', function() {
-        //     console.log('stream ended');
-        // });
+        console.log(version);
+
         // updateVersion(packageJson, version);
         updateVersion(einsatzverwaltung, version);
-        // updateVersion(readme, version);
+        updateVersion(readme, version);
 
         // console.log(packageJson+'\n'+einsatzverwaltung+'\n'+readme);
     });
-
-    // var read = function() {
-    //     var buf;
-    //     while (buf = stream.read()) {
-    //         console.log('Read from the file:', buf);
-    //     }
-    // };
 
     var readFile = function(file){
         return fs.readFileSync(file,'utf8');
     };
 
     var writeFile = function(filePath, content){
-        fs.writeFileSync(fileName, content, 'utf8');
+        fs.writeFileSync(filePath, content, 'utf8');
     };
 
-    var updateVersion = function(file, filePath, newVersion){
+    var updateVersion = function(file, newVersion){
         var regex = /\d*.\.\d*.\.\d*/;
 
 
 
 
 
-        var oldVersion = regex.exec(file);
+        var oldVersion = regex.exec(file.content);
         console.log('old version: '+oldVersion);
         console.log('new version: '+newVersion);
 
-        file = file.replace(regex, newVersion);
+        file.content = file.content.replace(regex, newVersion);
 
-        console.log(file);
+        console.log(file.content);
 
         // return version;
     };
 
     var incrementVersion = function(file){
 
-        var version = file.version.split('.');
+        var version = file.content.version.split('.');
         var versionNumber = parseInt(version[2], 10);
         versionNumber++;
-        return version[0]+'.'+version[1]+'.'+versionNumber;
+
+        var retVal = version[0]+'.'+version[1]+'.'+versionNumber;
+
+        file.content['version'] = retVal;
+    
+        fs.writeFileSync(file.filePath, JSON.stringify(file.content, null, 4));
+
+        return retVal;
+    };
+
+    var addFilesToGit = function(){
+
+        exec('git add package.json && git add einsatzverwaltung.php && git add Readme.md', function(error, stdout, stderr){
+            commitPackageJsonToGit();
+
+        });
+
+    };
+
+    var commitFilesToGit = function(){
+        exec('git commit -m "raised version"', function(error, stdout, stderr){
+
+            if(stdout !== null){
+                console.info(stdout);
+            }else{
+                console.info('stderr: ' + stderr);
+            }
+
+            if (error !== null) {
+                console.info('exec error: ' + error);
+            }
+        });
     };
 };
-
-
-// module.exports = function versioning() {
-
-//     exports.incrementVersion = function(){
-
-//         var packageJson = grunt.file.readJSON();
-//         var einsatzverwaltung;
-//         var readme;
-
-
-
-//     };
-
-
-//     return exports;
-// };
