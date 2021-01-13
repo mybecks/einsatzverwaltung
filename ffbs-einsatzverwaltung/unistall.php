808044a417b34e30d1eb62dd03588787757f741a
Andre Becker <andre.becker@posteo.de>
2021-01-13 08:36:13 +0100

    update

--- ffbs-einsatzverwaltung/unistall.php
+++ ffbs-einsatzverwaltung/unistall.php
@@ -2,24 +2,47 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-global $wpdb;
-$table_vehicles              = $wpdb->prefix . "fahrzeuge";
-$table_missions              = $wpdb->prefix . "einsaetze";
-$table_missions_has_vehicles = $wpdb->prefix . "einsaetze_has_fahrzeuge";
+function ffbs_einsatzverwaltung_uninstall_plugin () {
 
-// First clean up all custom posts
-$custom_posts = get_posts( array( 'post_type' => 'mission', 'numberposts' => -1 ) );
-foreach( $custom_posts as $post ) {
-    // Delete's each post.
-    wp_delete_post( $post->ID, true );
+    if ( ! current_user_can( 'activate_plugins' ) ) {
+        return;
+    }
+
+    delete_custom_posts();
+    drop_tables();
 }
 
-//Second remove plugin specific tables
-$sql_missions_has_vehicles = "DROP TABLE $table_missions_has_vehicles";
-dbDelta( $sql_missions_has_vehicles );
 
-$sql_vehicles = "DROP TABLE $table_vehicles";
-dbDelta( $sql_vehicles );
+function delete_custom_posts () {
+    register_post_type( 'mission' );
+    // fetch posts of type mission
+    $args = array(
+            'post_type' => 'mission'
+    );
 
-$sql_missions = "DROP TABLE $table_missions";
-dbDelta( $sql_missions );
+    $query = new WP_Query( $args );
+    while ( $query->have_posts() ) {
+        $query->the_post();
+        if ( 'mission' == get_post_type() ) {
+            $post_id = get_the_ID();
+            wp_delete_post( $post_id, true );
+        }
+    }
+    wp_reset_postdata();
+}
+
+function drop_tables () {
+    global $wpdb;
+    $table_vehicles              = $wpdb->prefix . "fahrzeuge";
+    $table_missions              = $wpdb->prefix . "einsaetze";
+    $table_missions_has_vehicles = $wpdb->prefix . "einsaetze_has_fahrzeuge";
+
+    $sql_missions_has_vehicles = "DROP TABLE $table_missions_has_vehicles";
+    dbDelta( $sql_missions_has_vehicles );
+
+    $sql_vehicles = "DROP TABLE $table_vehicles";
+    dbDelta( $sql_vehicles );
+
+    $sql_missions = "DROP TABLE $table_missions";
+    dbDelta( $sql_missions );
+}
