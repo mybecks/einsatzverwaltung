f2413e1e6a9159ce70961865620ca179dc88c69c
Andre Becker <andre.becker@posteo.de>
2021-02-11 08:20:24 +0100

    adding new naming of db tables

--- ffbs-einsatzverwaltung/uninstall.php
+++ ffbs-einsatzverwaltung/uninstall.php
@@ -1,23 +1,26 @@
 <?php
 
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
 }
 
 ffbs_einsatzverwaltung_uninstall_plugin();
 
-function ffbs_einsatzverwaltung_uninstall_plugin () {
+function ffbs_einsatzverwaltung_uninstall_plugin()
+{
 
-    if ( ! current_user_can( 'activate_plugins' ) ) {
+    if (!current_user_can('activate_plugins')) {
         return;
     }
 
     delete_custom_posts();
     drop_tables();
+    drop_tables_old();
 }
 
 
-function delete_custom_posts () {
+function delete_custom_posts()
+{
 
     // fetch posts of type mission
     $args = array(
@@ -25,19 +28,19 @@ function delete_custom_posts () {
         'numberposts' => -1
     );
 
-    $custom_posts = get_posts( $args );
+    $custom_posts = get_posts($args);
 
-    foreach( $custom_posts as $post ) {
+    foreach ($custom_posts as $post) {
         // Delete's each post.
-        wp_delete_post( $post->ID, true );
+        wp_delete_post($post->ID, true);
     }
 
-    unregister_post_type( 'mission' );
+    unregister_post_type('mission');
     wp_reset_postdata();
 }
 
-function drop_tables () {
-
+function drop_tables_old()
+{
     global $wpdb;
 
     $table_vehicles              = $wpdb->prefix . "fahrzeuge";
@@ -45,11 +48,29 @@ function drop_tables () {
     $table_missions_has_vehicles = $wpdb->prefix . "einsaetze_has_fahrzeuge";
 
     $sql_missions_has_vehicles = "DROP TABLE IF EXISTS $table_missions_has_vehicles";
-    $wpdb->query( $sql_missions_has_vehicles );
+    $wpdb->query($sql_missions_has_vehicles);
 
     $sql_vehicles = "DROP TABLE IF EXISTS $table_vehicles";
-    $wpdb->query( $sql_vehicles );
+    $wpdb->query($sql_vehicles);
 
     $sql_missions = "DROP TABLE IF EXISTS $table_missions";
-    $wpdb->query( $sql_missions );
+    $wpdb->query($sql_missions);
+}
+
+function drop_tables()
+{
+    global $wpdb;
+
+    $table_vehicles              = $wpdb->prefix . "ffbs_vehicles";
+    $table_missions              = $wpdb->prefix . "ffbs_missions";
+    $table_moved_out_vehicles = $wpdb->prefix . "ffbs_moved_out_vehicles";
+
+    $sql_missions_has_vehicles = "DROP TABLE IF EXISTS $table_moved_out_vehicles";
+    $wpdb->query($sql_missions_has_vehicles);
+
+    $sql_vehicles = "DROP TABLE IF EXISTS $table_vehicles";
+    $wpdb->query($sql_vehicles);
+
+    $sql_missions = "DROP TABLE IF EXISTS $table_missions";
+    $wpdb->query($sql_missions);
 }
