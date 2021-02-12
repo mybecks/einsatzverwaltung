18b6c86a60792eac32a445a33682b417326e5d96
mybecks <andre.becker@posteo.de>
2021-02-12 09:46:50 +0100

    update plugin author

--- ffbs-einsatzverwaltung/ffbs-einsatzverwaltung.php
+++ ffbs-einsatzverwaltung/ffbs-einsatzverwaltung.php
@@ -4,22 +4,22 @@ Plugin Name: FFBS Einsatzverwaltung
 Plugin URI: https://github.com/mybecks/einsatzverwaltung
 Description: Einsatzverwaltung der FF Bad Schönborn
 Version: 0.1.2
-Author: Andre Becker
+Author: FF Bad Schönborn
 Author URI: ffbs.de
 License: MIT
 */
 
-define( 'MISSIONS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define('MISSIONS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-require_once( MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung.php'               );
-require_once( MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-db.php'        );
-require_once( MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-admin.php'          );
-require_once( MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-custom-post.php' );
-require_once( MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-widget.php'          );
-require_once( MISSIONS_PLUGIN_DIR . 'ffbs-einsatzverwaltung-constants.php'          );
+require_once(MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung.php');
+require_once(MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-db.php');
+require_once(MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-admin.php');
+require_once(MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-custom-post.php');
+require_once(MISSIONS_PLUGIN_DIR . 'class.ffbs-einsatzverwaltung-widget.php');
+require_once(MISSIONS_PLUGIN_DIR . 'ffbs-einsatzverwaltung-constants.php');
 
-register_activation_hook( __FILE__, 'plugin_activation' );
-register_deactivation_hook( __FILE__, 'plugin_deactivation' );
+register_activation_hook(__FILE__, 'plugin_activation');
+register_deactivation_hook(__FILE__, 'plugin_deactivation');
 
 $wpEinsatzverwaltung = new Einsatzverwaltung();
 
